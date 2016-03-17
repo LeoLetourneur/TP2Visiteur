a4066c7e41aa09e910dd0f2f0cd045c723cc8e55
Letourneur Léo <letourneur.leo@gmail.com>
2016-03-17 17:56:43 +0100

    Correction bullshit

--- src/arbrebinaire/VisiteurInfixe.java
+++ src/arbrebinaire/VisiteurInfixe.java
@@ -7,33 +7,33 @@ public class VisiteurInfixe implements Visiteur {
 	@Override
 	public void visiterConstante(Constante c) {
 		System.out.print(c.getValeur());
-		if(parenthese) {
-			parenthese = false;
-			System.out.print(")");
-		}
 	}
 
 	@Override
 	public void visiterOperateurNegation(Negation n) {
 		System.out.print("("+n.getOp());
-		parenthese = true;
 		n.getOpG().accept(this);
+		System.out.print(")");
 	}
 
 	@Override
 	public void visiterOperateurAddition(Addition a) {
-		System.out.print("(");
+		if(parenthese)
+			System.out.print("(");
 		a.getOpG().accept(this);
 		System.out.print(a.getOp());
 		a.getOpD().accept(this);
-		System.out.print(")");
+		if(parenthese)
+			System.out.print(")");
 		
 	}
 
 	@Override
 	public void visiterOperateurMultiplication(Multiplication m) {
+		parenthese = true;
 		m.getOpG().accept(this);
 		System.out.print(m.getOp());
 		m.getOpD().accept(this);
+		parenthese = false;
 	}
 }
--- src/test/TestArbreBinaire.java
+++ src/test/TestArbreBinaire.java
@@ -13,6 +13,7 @@ public class TestArbreBinaire {
 	 */
 	public static void main(String[] args) {
 		//faire 1+2*3+-4=3
+		System.out.println("\n Pour 1+2*3+-4=3 ");
 		Addition racine = new Addition(new Addition(new Constante(1),
 					new Multiplication(new Constante(2),new Constante(3))),
 					new Negation(new Constante(4)));
@@ -23,5 +24,48 @@ public class TestArbreBinaire {
 		System.out.println("\n calcul hauteur: " + exp.calculerHauteur());
 		exp.afficherInFixe();
 		
+		//faire (1+2)*4=12
+		System.out.println("\n\n -------------------- ");
+		System.out.println("\n Pour (1+2)*4=12 ");
+		Multiplication racine2 = new Multiplication(
+									new Addition(
+										new Constante(1),
+										new Constante(2)
+									),
+									new Constante(4)
+								);
+		exp = new ExpressionArithmetique(racine2);
+
+		exp.afficherPostFixe();
+		System.out.println("\n\n calcul valeur: " + exp.calculerValeur());
+		System.out.println("\n calcul hauteur: " + exp.calculerHauteur());
+		exp.afficherInFixe();
+		
+		//faire ((2+3)*2+2)+2*4=20
+		System.out.println("\n\n -------------------- ");
+		System.out.println("\n Pour ((2+3)*2+2)*4+2=50 ");
+		Addition racine3 = 		new Addition(
+									new Multiplication(
+										new Addition(
+											new Multiplication(
+												new Addition(
+													new Constante(2),
+													new Constante(3)
+												),
+												new Constante(2)
+											),
+											new Constante(2)
+										),
+										new Constante(4)
+									),
+									new Constante(2)
+								);
+		exp = new ExpressionArithmetique(racine3);
+
+		exp.afficherPostFixe();
+		System.out.println("\n\n calcul valeur: " + exp.calculerValeur());
+		System.out.println("\n calcul hauteur: " + exp.calculerHauteur());
+		exp.afficherInFixe();
+				
 	}
 }
